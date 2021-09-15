55feb98e6053c741235e941b4c4409840e28e92a
johnconnor <zkmd2012@inbox.ru>
2021-09-15 12:11:28 +0500

    02. Two ways of sending status codes explained

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res, next) => {
-    console.log('Here')
-    res.send('Hi')
+    //res.sendStatus(500)  //also one of the ways to send Status codes
+    res.status(500).send("There is an error on a server")  //sending status code with custom message
 })
 
 app.listen(3000, () => console.log("Server runs on port 3000"))
