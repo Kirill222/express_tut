c455d9817caf724bb09c53cb0dd76a468626c55a
johnconnor <zkmd2012@inbox.ru>
2021-09-15 14:32:10 +0500

    Getting parameters from the URL, order of the methods explained

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,28 @@ router.get('/', (req, res, next) => {
     res.send("User list")
 })
 
-router.get('/new', (req, res, next) => {
+router.get('/new', (req, res, next) => {  //static routes should be above dynamic ones
     res.send("New user")    
 })
+
+router.post('/', (req, res, next) => {
+    res.send('Create user')
+})
+
+router.get('/:userId', (req, res, next) => {
+    const id = req.params.userId
+    res.send(`Get user with id ${id}`)
+})
+
+router.put('/:userId', (req, res, next) => {
+    const id = req.params.userId
+    res.send(`Update ${id}`)
+})
+
+router.delete('/:userId', (req, res, next) => {
+    const id = req.params.userId
+    res.send(`Delete id ${id}`)
+})
+
 
 module.exports = router
