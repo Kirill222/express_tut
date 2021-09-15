66affac96c2f6b2a7ffa775ee0cc960d353372f5
johnconnor <zkmd2012@inbox.ru>
2021-09-15 12:37:25 +0500

    04. View engines, EJS, res.render() explained

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express')
 
 const app = express()
 
+//setting a view engine - ejs, another one could be PUG
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res, next) => {
-    //res.sendStatus(500)  //also one of the ways to send Status codes
-    //res.status(500).send("There is an error on a server")  //sending status code with custom message
-    //res.status(200).json({message: "hi"})
-    //res.json({message: "hi"}) //the default is 200, so we do not need to use status code explicitly
-    res.download("server.js") //DOWNLOADING FILES
+    res.render('index')  //when using view engine, the defaul falder or these files is "views/"
 })
 
 app.listen(3000, () => console.log("Server runs on port 3000"))
