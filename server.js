c077c77eb51e8e9292e42d0a7c7d945e504cab1b
johnconnor <zkmd2012@inbox.ru>
2021-09-15 12:18:49 +0500

    03. How to send JSON data and how to DOWNLOAD files explained

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const app = express()
 
 app.get('/', (req, res, next) => {
     //res.sendStatus(500)  //also one of the ways to send Status codes
-    res.status(500).send("There is an error on a server")  //sending status code with custom message
+    //res.status(500).send("There is an error on a server")  //sending status code with custom message
+    //res.status(200).json({message: "hi"})
+    //res.json({message: "hi"}) //the default is 200, so we do not need to use status code explicitly
+    res.download("server.js") //DOWNLOADING FILES
 })
 
 app.listen(3000, () => console.log("Server runs on port 3000"))
